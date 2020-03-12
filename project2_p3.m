3087ba2f299950f1282dc0145d763399b8f4d89d
mrr24 <60560444+mrr24@users.noreply.github.com>
2020-03-11 17:49:35 -0700

    Update project2_p3.m

--- project2_p3.m
+++ project2_p3.m
@@ -1,32 +1,28 @@
-
-
-
+format long
 
 % Give several pairs of InputList and InputWeight values
- C1 = [1; 2; 3];
- w1 = [1 2 3];
- Neuron(C1, w1)
-% This gives the case where NET = 14 OUT = 9.999991684719722e-01
+C1 = [10; 20; 30];
+w1 = [10 20 30];
+Neuron(C1, w1)
+% NET = 1400, OUT = 1
 
- C2 = [.1;.1;.1];
- w2 = [.2 .2 .2];
- Neuron(C2, w2)
-% This gives the case where NET = 6.000000000000001e-02 and the
-% corresponding OUT = 5.149955016194100e-01
+C2 = [.1;.1;.1];
+w2 = [.2 .2 .2];
+Neuron(C2, w2)
+% NET = 0.06, OUT = 0.514995501619410
+ 
+C3 = [.01;.01;.01];
+w3 = [.02 .02 .02];
+Neuron(C3, w3)
+% NET = 6.000000000000001e-04, OUT =  0.500149999995500
 
- C3 = [.01;.01;.01];
- w3 = [.02 .02 .02];
- Neuron(C3, w3)
-% This gives NET = 6.000000000000001e-04 and the OUT = 5.001499999954999e-01
-
-% From the above three cases we observe that for F = logistics function, as
-% NET gets smaller, the value of OUT is getting smaller too. This implies
-% that smalle value of NET gives small value of OUT and vice versa.
+% Given F = sigmoidal (logistic) function:
+% As the values of NET decrease, so do the OUT values
 
 function [ OUT ] = Neuron(C, w)
 %INPUT: n input connections, "C"
 %       n input weights "w"
 % NET is summation of two matrices
-NET = w * C;
+NET = w * C
 OUT = 1/(1+exp(-NET));
 end
