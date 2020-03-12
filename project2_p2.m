7fbb06e7f7db7e545cc9a3fe68010a7d6c9cd6b9
mrr24 <60560444+mrr24@users.noreply.github.com>
2020-03-11 17:26:55 -0700

    Add files via upload

---
+++ project2_p2.m
@@ -0,0 +1,27 @@
+%Read digits from data base.
+%Type lines 3-6 in command window to produce a zero-image.
+digit = train0(1,:);
+digitimage = reshape(digit, 28, 28); %little i
+image(rot90(flipud(digitimage),-1));
+colormap(gray(256)), axis square tight off
+
+%Compute average digit (from 17a)
+%plot them and compare with p. 180.
+T(1,:) = mean(train0);
+T(2,:) = mean(train1);
+T(3,:) = mean(train2);
+T(4,:) = mean(train3);
+T(5,:) = mean(train4);
+T(6,:) = mean(train5);
+T(7,:) = mean(train6);
+T(8,:) = mean(train7);
+T(9,:) = mean(train8);
+T(10,:) = mean(train9);
+
+for j = 1:10
+    subplot(2,5,j);
+    i = T(j,:);
+    digitImage = reshape(i, 28, 28); %big I
+    image(rot90(flipud(digitImage),-1));
+    colormap(gray(256)), axis square tight off
+end
