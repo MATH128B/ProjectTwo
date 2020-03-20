3df44a4a444614aff457976e81b01584f407b63a
mrr24 <60560444+mrr24@users.noreply.github.com>
2020-03-19 17:30:17 -0700

    Update project2_p3.m

--- project2_p3.m
+++ project2_p3.m
@@ -1,28 +1,27 @@
-format long
+format long    %compute with "long" number of sig digits
 
-% Give several pairs of InputList and InputWeight values
+% Examine relationships of connections and weights
 C1 = [10; 20; 30];
 w1 = [10 20 30];
 Neuron(C1, w1)
 % NET = 1400, OUT = 1
 
-C2 = [.1;.1;.1];
-w2 = [.2 .2 .2];
+C2 = [1;1;1];
+w2 = [2 2 2];
 Neuron(C2, w2)
-% NET = 0.06, OUT = 0.514995501619410
+% NET = 6, OUT = 0.997527376843365
  
-C3 = [.01;.01;.01];
-w3 = [.02 .02 .02];
+C3 = [.1;.1;.1];
+w3 = [.2 .2 .2];
 Neuron(C3, w3)
-% NET = 6.000000000000001e-04, OUT =  0.500149999995500
+% NET = 0.060..., OUT = 0.514995501619410
 
 % Given F = sigmoidal (logistic) function:
 % As the values of NET decrease, so do the OUT values
 
-function [ OUT ] = Neuron(C, w)
+function [OUT] = Neuron(C, w)
 %INPUT: n input connections, "C"
 %       n input weights "w"
-% NET is summation of two matrices
 NET = w * C
-OUT = 1/(1+exp(-NET));
+OUT = 1/(1+exp(-NET)); %Function we were given
 end
